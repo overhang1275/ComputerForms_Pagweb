447d370a4e2ee216e4581d5768a673385d961035
Anthony Tepach <anthony.tepach@gmail.com>
2026-06-25 12:05:17 -0600

    feat: add sitemap integration and update robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "static",
-  integrations: [tailwind()],
+  site: "https://computerforms.com.mx",
+  integrations: [tailwind(), sitemap()],
 });
